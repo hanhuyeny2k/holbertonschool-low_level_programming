2c6c47e50a6cdd62a7dacd722af11b64da0a90c1
Hanhuyeny2k <hanhuyeny2k@gmail.com>
2019-06-20 01:52:12 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - using puts
- */
+ * main - entry point
+ * Description: using puts
+ * Return: 0
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
