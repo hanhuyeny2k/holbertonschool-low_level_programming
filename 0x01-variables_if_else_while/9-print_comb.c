38cecfcb61c598354b4e84738662bea9c2c1f5ad
Hanhuyeny2k <hanhuyeny2k@gmail.com>
2019-06-21 02:16:30 +0000

    code

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: using printf
+ * Return: 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 48; ch <= 57; ch++)
+	{
+		putchar(ch);
+		if (ch != 57)
+		{
+		putchar(',');
+		putchar(' ');
+		}	
+	}
+	putchar('\n');
+	return (0);
+
+}
