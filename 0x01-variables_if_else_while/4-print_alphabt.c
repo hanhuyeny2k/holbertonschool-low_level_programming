254d05bc1d827ff07a7b670a0da5d630672b0e56
Hanhuyeny2k <hanhuyeny2k@gmail.com>
2019-06-21 01:02:50 +0000

    assci

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,10 +9,12 @@ int main(void)
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-	if (ch != 101 && ch != 103)
-{
-	putchar(ch);
-}
+	{
+		if (ch != 101 && ch != 113)
+		{
+			putchar(ch);
+		}
+	}
 	putchar('\n');
 
 	return (0);
