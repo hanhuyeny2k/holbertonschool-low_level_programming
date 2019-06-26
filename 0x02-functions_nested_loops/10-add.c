f7f1c1549505182330f465258ac6fa1119da20e0
Hanhuyeny2k <hanhuyeny2k@gmail.com>
2019-06-26 22:31:48 +0000

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * add - entry point
+ * @a: integer
+ * @b: integer
+ * Description: counting hours
+ * Return: 0
+ */
+int add(int a, int b)
+{
+	int n;
+
+	n = a + b;
+	return (n);
+}
