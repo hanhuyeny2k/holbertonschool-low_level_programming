28a227ee9b4692acea8457419ee1fa88b98af88a
Hanhuyeny2k <hanhuyeny2k@gmail.com>
2019-06-26 00:15:02 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_alphabet - check the code for Holberton School students.
+ * description: learning prototype
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
