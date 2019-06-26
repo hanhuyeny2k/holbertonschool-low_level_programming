3e4b2585f77b2e577a356bc61f62364679a78830
Hanhuyeny2k <hanhuyeny2k@gmail.com>
2019-06-26 04:00:51 +0000

    times

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * times_table - entry point
+ * Description: counting hours
+ * Return: 0
+ */
+void times_table(void)
+{
+	int x;
+	int y;
+	int result;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			result = x * y;
+			if (result / 10 == 0)
+			{
+				_putchar(' ');
+				_putchar(result % 10 + '0');
+			}
+			else
+			{
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
+			}
+			if (y != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
+
