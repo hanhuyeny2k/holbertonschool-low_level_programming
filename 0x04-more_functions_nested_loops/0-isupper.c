1558e3773b16625f181c04c9f4354ce20ade7b56
Hanhuyeny2k <hanhuyeny2k@gmail.com>
2019-06-27 16:43:48 +0000

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _isupper - entry point
+ * @c: upper case integer
+ * Description: checks for uppercase character
+ * Return: 0
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
