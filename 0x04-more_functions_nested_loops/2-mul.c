e17eeee91c58f22ea73d816661d6fc541a733071
Hanhuyeny2k <hanhuyeny2k@gmail.com>
2019-06-27 22:25:29 +0000

    multiple by 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * mul - entry point
+ * @a: variables for integer
+ * @b: variables for integer
+ *
+ * Description: multiplies two integers
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
