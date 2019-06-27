10da1700c86f444109375f87eae969f772cd5675
Hanhuyeny2k <hanhuyeny2k@gmail.com>
2019-06-27 21:23:48 +0000

    >14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * more_numbers - entry point
+ *
+ * Description: print 10 times the numbers
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 10; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b > 9)
+			{
+				_putchar('0' + b / 10);
+				_putchar('0' + b % 10);
+			}
+			else
+			{	
+				_putchar ('0' + b);
+			}
+		}
+		_putchar('\n');
+	}
+}
