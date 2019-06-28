3c39ece0f9018c9f7b4fedbe177bdaccee1622a8
Hanhuyeny2k <hanhuyeny2k@gmail.com>
2019-06-28 01:27:41 +0000

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_square - entry point
+ * @size: variable integer
+ *
+ * Description: print a square
+ * Return: 0
+ */
+void print_square(int size)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < size; a++)
+	{
+		for (b = 0 ; b < size; b++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
+
