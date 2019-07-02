d324fffe09c1b54d705dc9ce549e57430549d839
Hanhuyeny2k <hanhuyeny2k@gmail.com>
2019-07-02 17:13:08 +0000

    q#

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * puts2 - entry point
+ * @str: parameter for a variable
+ *
+ * Description: prints one char out of 2 of a string
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; (i = i + 2))
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
