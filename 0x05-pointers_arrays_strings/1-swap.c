f4cd51a25728af4db3d53722d2a391870fd1d081
Hanhuyeny2k <hanhuyeny2k@gmail.com>
2019-07-02 14:27:59 +0000

    swap pointer

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * swap_int - entry point
+ * @a: parameter of a digit
+ * @b: parameter of a digit
+ *
+ * Description: swap integers
+ * Return: 0
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
