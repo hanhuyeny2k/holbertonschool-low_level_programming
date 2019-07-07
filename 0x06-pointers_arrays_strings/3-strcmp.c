0309dfb5765b1d8705ecc2a3afa7e4f3a41415d3
Hanhuyeny2k <hanhuyeny2k@gmail.com>
2019-07-07 06:24:43 +0000

    difference of two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * _strcmp - entry point
+ * @s1: pointer
+ * @s2: pointer
+ *
+ * Description: compares two strings
+ * Return: the difference or zero if no difference
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+	else if (s1[i] == s2[i])
+		return (0);
+	else if (s1[i] == '\0')
+		return (-1);
+	else
+		return (1);
+	return (0);
+}
