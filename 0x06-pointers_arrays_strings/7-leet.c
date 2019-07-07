bc09937803f917cd32e60cf9c578bc3b6db61b54
Hanhuyeny2k <hanhuyeny2k@gmail.com>
2019-07-07 05:58:44 +0000

    switch letters to number

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * leet - entry point
+ * @s: pointer
+ *
+ * Description: replacing a & A, e & E, o & O, t & T, l & L with 4, 3, 0, 7, 1, respectively. 
+ * Return: s
+ */
+char *leet(char *s)
+{
+	int i;
+	int j;
+
+	char upper[] = {'A', 'E', 'O', 'T', 'L'};
+	char lower[] = {'a', 'e', 'o', 't', 'l'};
+	char number[] = {'4', '3', '0', '7', '1'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == upper[j] || s[i] == lower[j])
+				s[i] = number[j];
+		}
+	}
+	return (s);
+}
