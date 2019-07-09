c9e7c5b80e428600c3bb935daa11a35cac302d5d
Hanhuyeny2k <hanhuyeny2k@gmail.com>
2019-07-09 21:37:41 +0000

    locate a char in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strchr - entry point
+ * @s: pointer to an arryay
+ * @c: assigned character
+ *
+ * Description: locates a character in a string
+ * Return: s or null
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{	
+		if (s[i] == c)
+			return (s + i);
+	}
+	return (0);
+}
