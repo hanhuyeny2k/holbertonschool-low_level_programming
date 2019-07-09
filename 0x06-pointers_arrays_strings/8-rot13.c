0645c07a4eb032e71eba9521b17fe18416c02c8e
Hanhuyeny2k <hanhuyeny2k@gmail.com>
2019-07-09 00:04:30 +0000

    rot13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * rot13 - entry point
+ * @s: pointer
+ *
+ * Description: encode a string using rot13
+ * Return: s
+ */
+char *rot13(char *s)
+{
+	int i;
+	int j;
+
+	char u1[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+			'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+			'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+			'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	char l1[] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+			'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+			'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+			'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == u1[j])
+			{
+				s[i] = l1[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
