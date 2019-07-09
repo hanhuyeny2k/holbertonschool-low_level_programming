2102f443c18dfb987609d0a66abb6544abf0f793
Hanhuyeny2k <hanhuyeny2k@gmail.com>
2019-07-09 20:10:30 +0000

    copy memory

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _memcpy - entry point
+ * @dest: pointer to an arryay
+ * @src: pointer to an array
+ * @n: number assign randomly
+ *
+ * Description: copy memory area
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	return (dest);
+}
