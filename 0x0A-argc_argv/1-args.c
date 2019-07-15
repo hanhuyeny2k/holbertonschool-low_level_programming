85748bb89e2c9042af911de0a0a392f3a9c7714f
Hanhuyeny2k <hanhuyeny2k@gmail.com>
2019-07-15 03:34:19 +0000

    prints the argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argv: an array containing program arguments
+ * @argc: the size of the argv array
+ *
+ * Description: a program that prints the number of arguments passed into it
+ * Return: zero
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}
