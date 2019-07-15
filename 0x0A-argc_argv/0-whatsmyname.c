58b2f0c567db95a7aaab56aa307373aecd1b5262
Hanhuyeny2k <hanhuyeny2k@gmail.com>
2019-07-15 03:29:06 +0000

    print its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/**
+ * main - entry point
+ * @argv: an array containing program arguments
+ * @argc: the size of the argv array
+ *
+ * Description: a program that prints its name
+ * Return: zero
+ */
 
 int main (int argc, char *argv[])
 {
