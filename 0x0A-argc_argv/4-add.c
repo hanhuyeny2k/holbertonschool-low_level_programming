944579303738bb9c31ee8c655f03aebf7098236a
Hanhuyeny2k <hanhuyeny2k@gmail.com>
2019-07-16 02:21:21 +0000

    addition

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
  * main - entry point
  * @argv: an array containing program arguments
@@ -11,24 +12,26 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
+	int j, i;
 	int sum = 0;
 
-	if (argc == 0)
-		printf("0");
+	if (argv[1] == '\0')
+	{
+		printf("0\n");
+		return (0);
+	}
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]) > 0)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			sum = sum + atoi(argv[i]);
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
+		sum = sum + atoi(argv[i]);
 	}
-
 	printf("%d\n", sum);
 	return (0);
 }
