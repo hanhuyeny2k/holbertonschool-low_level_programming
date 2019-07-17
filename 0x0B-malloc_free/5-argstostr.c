8819c383b1e638e7060411b05df4e206592d23a7
Hanhuyeny2k <hanhuyeny2k@gmail.com>
2019-07-17 04:11:49 +0000

    concatenate with argc and argv

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * argstostr - main entry
+ * @ac: value given from main
+ * @av: pointer for a string
+ *
+ *Description: concatenate all the arguments of the program
+ * Return: pointer arg
+ */
+char *argstostr(int ac, char **av)
+{
+	int i;
+	int j;
+	int k = 0;
+	int size = 0;
+	char *arg = NULL;
+
+	if (ac == 0)
+		return (NULL);
+	if (av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			size++;
+		}
+		size++;
+	}
+	arg = malloc(sizeof(char) * size);
+	if (arg == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			arg[k] = av[i][j];
+			k++;
+		}
+		arg[k] = '\n';
+		k++;
+	}
+	return (arg);
+}
+
+
