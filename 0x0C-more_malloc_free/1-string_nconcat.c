e426834c7af45ff6d57da5d043b5695f30609c95
Hanhuyeny2k <hanhuyeny2k@gmail.com>
2019-07-18 22:41:51 +0000

    concat 2 strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,7 +23,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i] != '\0'; i++)
 		;
 	for (j = 0; s2[j] != '\0'; j++)
-		sum = (i + n);
+		;
+		if (n > j || n == j)
+		{
+			n = j;
+			sum = (i + n);
+		}
+		else
+			sum = (i + n);
 	a = malloc(sizeof(char) * (sum + 1));
 		if (a == NULL)
 			return (NULL);
