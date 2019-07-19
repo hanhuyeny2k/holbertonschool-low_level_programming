c4cb776db25bf5e6f9d07952b2543c93976cd1f1
Hanhuyeny2k <hanhuyeny2k@gmail.com>
2019-07-19 00:57:35 +0000

    calloc tomalloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _calloc - main entry
+ * @nmemb: number of elements
+ * @size: bytes size
+ *
+ *Description: allocate memory for calloc using malloc
+ * Return: pointer a
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *a;
+
+	if (nmemb == 0)
+		return (NULL);
+	if (size == 0)
+		return (NULL);
+	a = malloc(nmemb * size);
+	if (a == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		a[i] = 0;
+	return (a);
+}
