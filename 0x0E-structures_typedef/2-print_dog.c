6edf4abb48c7a15ccd70736932fa67e8ddd90c60
Hanhuyeny2k <hanhuyeny2k@gmail.com>
2019-07-23 00:14:33 +0000

    print dog's tag

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_dog - entry point
+ * @d: pointer to struct dog
+ *
+ * Return: nothing if null
+ */
+void print_dog(struct dog *d)
+{
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n)");
+	else
+		printf("Owner: %s\n", d->owner);
+	if (d == NULL)
+		return;
+}
