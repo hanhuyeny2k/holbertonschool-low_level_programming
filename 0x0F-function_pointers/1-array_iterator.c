a3a06ad7035c2ad268dfa57f310f77bb063ab706
Hanhuyeny2k <hanhuyeny2k@gmail.com>
2019-07-24 23:44:27 +0000

    print array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * array_iterator - main entry
+ *@array: given array
+ *@size: size of the array
+ *@action: function pointer
+ *
+ * Return: 0
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL)
+		return;
+	if (action == NULL)
+		return;
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
