c0cfb9ea0286058dcf947e2e2d1f4b74c44e6a76
Hanhuyeny2k <hanhuyeny2k@gmail.com>
2019-07-26 20:00:09 +0000

    sum-up

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - main entry, use this function to print
+ * @n: given a list
+ * @...: ellipses
+ *
+ * Return: the sum of the list
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum_up = 0;
+	va_list listo;
+
+	va_start(listo, n);
+	for (i = 0; i < n; i++)
+	{
+		if (n == 0)
+			return (0);
+			sum_up += va_arg(listo, int);
+	}
+	va_end(listo);
+	return (sum_up);
+}
