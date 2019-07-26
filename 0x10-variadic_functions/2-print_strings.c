c04b42ea8b2c40ca564b98a87b0d129f16c933d2
Hanhuyeny2k <hanhuyeny2k@gmail.com>
2019-07-26 22:01:03 +0000

    print names and commas

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_strings - main entry, use this function to print
+ * @separator: string to be printed between the strings
+ * @n: number of strings passed to the function
+ * @...: ellipses
+ *
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *combo_strings;
+	va_list strings;
+
+	va_start(strings, n);
+	if (separator == NULL)
+		separator = "";
+	for (i = 0; i < n; i++)
+	{
+		combo_strings = va_arg(strings, char*);
+		printf("%s", combo_strings);
+		if (1 + i != n)
+			printf("%s", separator);
+		else if (strings == 0)
+			printf("(nill)");
+	}
+	printf("\n");
+	va_end(strings);
+}
