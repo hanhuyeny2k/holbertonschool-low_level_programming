126963fc0aea441c3dcc61b5a31379fd5184fd4d
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-08-06 20:13:32 +0000

    insertion

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,6 +3,27 @@
 #include <stdio.h>
 
 /**
+ * add_nodeint - main entry, add a node at the beginning of linked list
+ * @head: pointer to the head of the node
+ * @n: integer contains inside the node
+ *
+ * Return: the counter
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
+
+/**
  * insert_nodeint_at_index - main entry, insert a new node at a given position
  * @head: double pointer to the head of the node
  * @idx: given position of index where new node be added
@@ -18,9 +39,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new;
 
 	ptr = *head;
-	if (head == NULL || *head == NULL)
+	if ((head == NULL || *head == NULL) && idx != 0)
 		return (NULL);
-	if (idx == 0)
+	if ((head == NULL || *head == NULL) && idx == 0)
+	{
+		new = add_nodeint(head, n);
+			return (new);
+	}
+	if (idx == 0 && (head != NULL || *head != NULL))
 	{
 		new = malloc(sizeof(listint_t));
 		new->n = n;
