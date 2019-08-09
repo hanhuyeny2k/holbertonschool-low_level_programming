19feaff91dbb30794375f5c1e4ed578911254d6c
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-08-09 22:27:07 +0000

    change bit to 0

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * clear_bit - main entry, set the value of a bit to 0 at a given index
+ * @n: pointer pointing the binary number
+ * @index: index that starts from 0
+ *
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int bit = 1;
+
+	if (index >= 64)
+		return (-1);
+	bit = bit << index;
+	*n = *n & ~bit;
+	return (1);
+}
