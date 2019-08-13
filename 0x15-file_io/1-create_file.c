7ebcbb4edd906c912268442bcacfd7da136e2822
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-08-13 22:27:43 +0000

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,6 +21,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		return (1);
 	for (i = 0; text_content[i] != '\0'; i++)
+	{}
 	wr = write(fd, text_content, i);
 	if (wr == -1)
 		return (-1);
