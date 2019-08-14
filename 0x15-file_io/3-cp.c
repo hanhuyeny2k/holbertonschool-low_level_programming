a303c60a04f6432fe28167de37df41746de80082
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-08-14 02:38:33 +0000

    copy file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,34 @@
 #include "holberton.h"
 /**
+ * helper_close - main entry, close the file
+ * @fd: file descriptor
+ *
+ */
+void helper_close(int fd)
+{
+	int cl;
+
+	cl = close(fd);
+	if (cl == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+/**
+ * helper_count - main entry, count the number of arguments
+ * @ac: number of arguments
+ *
+ */
+void helper_count(int ac)
+{
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+}
+/**
  * main - main entry, copy a file to another file
  * @ac: number of arguments
  * @av: the arguments
@@ -8,43 +37,44 @@
  */
 int main(int ac, char **av)
 {
-	int fd, fe, cl;
+	int fd, fe, wr, rd;
 	char buffer[1024];
 
-	if (ac != 3)
-	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+	helper_count(ac);
 	fd = open(av[2], O_CREAT | O_TRUNC | O_RDWR, 0664);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", av[2]);
+		exit(99);
+	}
 	fe = open(av[1], O_RDWR);
 	if (fe == -1)
 	{
 		dprintf(STDERR_FILENO,
 				"Error: Can't read from file %s\n", av[1]);
+		helper_close(fd);
 		exit(98);
 	}
-	if (fd == -1)
+	while ((rd = read(fe, buffer, 1024)))
 	{
-		dprintf(STDERR_FILENO,
-				"Error: Can't write from file %s\n", av[2]);
-		exit(99);
+		wr = write(fd, buffer, 1024);
+		if (wr == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", av[2]);
+			helper_close(fd);
+			helper_close(fe);
+			exit(99);
+		}
+		if (rd == -1)
+		{
+			dprintf(STDERR_FILENO,
+					"Error: Can't read from file %s\n", av[1]);
+			helper_close(fe);
+			helper_close(fd);
+			exit(98);
+		}
 	}
-	while (read(fe, buffer, 1024))
-	{
-		write(fd, buffer, 1024);
-	}
-	cl = close(fd);
-	if (cl == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
-	cl = close(fe);
-	if (cl == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fe);
-		exit(100);
-	}
+	helper_close(fd);
+	helper_close(fe);
 	return (0);
 }
