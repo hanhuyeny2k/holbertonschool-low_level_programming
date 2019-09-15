ce836e02b3bf32db348a52109dddc81e6ba3eb18
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-09-15 05:35:37 +0000

    return the number of elements

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - main entry, return the number of the elements from the list
+ * @h: pointer to the head of the node
+ *
+ * Return: the counter
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t counter = 0;
+	const dlistint_t *ptr;
+
+	ptr = h;
+	if (ptr == NULL)
+		return (0);
+	for (; ptr != NULL; ptr = ptr->next)
+		counter++;
+	return (counter);
+}
