9ff546d54312d6d2d825cc4d8ea43e881a7cabc1
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-10-30 21:39:09 +0000

    Fixed Betty error

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -9,27 +9,27 @@
 
 listint_t *swap_nodes(listint_t *node1, listint_t *node2)
 {
-        listint_t *temp;
+	listint_t *temp;
 
-        temp = node1->next;
-        node1->next = node2->next;
-        node2->next = temp;
+	temp = node1->next;
+	node1->next = node2->next;
+	node2->next = temp;
 
-        if (node1->next != NULL)
-                node1->next->prev = node1;
-        if (node2->next != NULL)
-                node2->next->prev = node2;
+	if (node1->next != NULL)
+		node1->next->prev = node1;
+	if (node2->next != NULL)
+		node2->next->prev = node2;
 
-        temp = node1->prev;
-        node1->prev = node2->prev;
-        node2->prev = temp;
+	temp = node1->prev;
+	node1->prev = node2->prev;
+	node2->prev = temp;
 
-        if (node1->prev != NULL)
-                node1->prev->next = node1;
-        if (node2->prev == NULL)
-                return (node2);
-        node2->prev->next = node2;
-        return (node1);
+	if (node1->prev != NULL)
+		node1->prev->next = node1;
+	if (node2->prev == NULL)
+		return (node2);
+	node2->prev->next = node2;
+	return (node1);
 }
 
 /**
