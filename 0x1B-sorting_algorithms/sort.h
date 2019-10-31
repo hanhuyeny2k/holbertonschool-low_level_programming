7046e756defbd96e8ea34f3b7001eb38f3b937f4
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-10-31 03:42:44 +0000

    Added extra functions to header

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -28,5 +28,7 @@ void merge_sort(int *array, size_t size);
 void counting_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
+int get_max(int *array, size_t size);
+void countingSort(int *array, int size, int place); 
 
 #endif
