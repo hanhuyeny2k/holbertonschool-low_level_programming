5cdb3a5dc7a035992d363e2a02170a2a2f6e3b3d
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-12-06 00:11:32 +0000

    removed n

--- 0x1D-binary_trees/18-binary_tree_uncle.c
+++ 0x1D-binary_trees/18-binary_tree_uncle.c
@@ -10,7 +10,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node == NULL || node->parent->parent == NULL)
 		return (NULL);
-	if (node->parent->n == node->parent->parent->right->n)
+	if (node->parent == node->parent->parent->right)
 		return (node->parent->parent->left);
 	else
 		return (node->parent->parent->right);
