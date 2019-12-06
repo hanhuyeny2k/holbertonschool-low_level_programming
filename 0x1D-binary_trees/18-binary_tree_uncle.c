60e827f7c0eaa9e95050962ceeef4831b6726909
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-12-06 00:18:38 +0000

    added the node->parent == NULL

--- 0x1D-binary_trees/18-binary_tree_uncle.c
+++ 0x1D-binary_trees/18-binary_tree_uncle.c
@@ -8,7 +8,8 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL || node->parent->parent == NULL)
+	if (node == NULL || node->parent == NULL
+		|| node->parent->parent == NULL)
 		return (NULL);
 	if (node->parent == node->parent->parent->right)
 		return (node->parent->parent->left);
